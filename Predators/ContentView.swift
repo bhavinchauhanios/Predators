2cd0cbb0c63fb8226ae8d729c9b6f3536ed1ce8f
Bhavin Chauhan <bhavin.9909@gmail.com>
2025-07-30 20:42:37 +0530

    Initial Commit

---
+++ Predators/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Predators
+//
+//  Created by Bhavin Chauhan on 30/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Predators/PredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PredatorsApp.swift
+//  Predators
+//
+//  Created by Bhavin Chauhan on 30/07/25.
+//
+
+import SwiftUI
+
+@main
+struct PredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
